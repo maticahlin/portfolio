f8c5b37facc32b2ed2b9e5757c5d6f92f6e98c79
Matic Ahlin <maticahlin2@gmail.com>
2026-04-23 09:43:02 +0200

    Last background push

--- app/HomeContent.tsx
+++ app/HomeContent.tsx
@@ -116,6 +116,7 @@ export default function HomeContent() {
         backgroundSize: 'cover', 
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
+        backgroundColor: '#141414'
       }}
     >
       <TopBar />
